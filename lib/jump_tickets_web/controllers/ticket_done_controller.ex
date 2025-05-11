2a5b5124b004c2c1343d7d91b5e1c9bbe99503eb
briancbarrow <briancbarrow@gmail.com>
2025-05-10 22:07:27 -0600

    update signature to check for specific "Done" field

--- lib/jump_tickets_web/controllers/ticket_done_controller.ex
+++ lib/jump_tickets_web/controllers/ticket_done_controller.ex
@@ -11,7 +11,19 @@ defmodule JumpTicketsWeb.TicketDoneController do
 
   Expects a JSON payload with the `page_id` key.
   """
-  def notion_webhook(conn, %{"page_id" => page_id}) do
+  def notion_webhook(
+        conn,
+        %{
+          "data" => %{
+            "parent" => %{
+              "id" => "1efde731-629e-8006-a973-d86b9265f008",
+              "type" => "database"
+            },
+            "updated_properties" => ["LQy%7B"]
+          },
+          "entity" => %{"id" => page_id}
+        } = params
+      ) do
     with %Ticket{} = ticket <- Notion.get_ticket_by_page_id(page_id),
          :ok <- DoneNotifier.notify_ticket_done(ticket) do
       json(conn, %{status: "ok", message: "Ticket done notification sent."})
@@ -22,5 +34,11 @@ defmodule JumpTicketsWeb.TicketDoneController do
         |> put_status(500)
         |> json(%{status: "error", error: inspect(error)})
     end
+  end
+
+  def notion_webhook(conn, params) do
+    conn
+    |> put_status(400)
+    |> json(%{status: "error", message: "Invalid payload"})
   end
 end
